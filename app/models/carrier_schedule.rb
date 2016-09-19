2d681e44e15150a068ef1d7b59d3a51eb536fef3
Shuhail <shukad33@yahoo.com>
2016-09-19 22:30:45 +0530

    rating with response

--- app/models/carrier_schedule.rb
+++ app/models/carrier_schedule.rb
@@ -3,6 +3,7 @@ class CarrierSchedule < ActiveRecord::Base
   has_one :carrier_schedule_detail , foreign_key: 'schedule_id' , autosave: true ,primary_key: 'schedule_id'
   #belongs_to :carrier_detail
   accepts_nested_attributes_for :carrier_schedule_detail
+  has_many :rating, :foreign_key => 'user' , :autosave => true , :primary_key => 'carrier_id'
 
   default_scope { order(updated_at: :desc) }
 
